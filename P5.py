31c9aec66464f5abccd3d318c0aceefb01666827
Priyank S Patel <pspoffice365@gmail.com>
2022-06-11 17:21:49 +0530

    Problem 5 Smallest Multiple

---
+++ P5.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+# https://projecteuler.net/problem=5
+
+# Smallest Multiple
+
+
+import math
+
+
+def main():
+    t = int(input().strip())
+    for _ in range(t):
+        n = int(input().strip())
+        result = 1
+        for x in range(2, n + 1):
+            print(result, x)
+            result = math.lcm(result, x)
+        print(result)
+
+
+if __name__ == '__main__':
+    main()
